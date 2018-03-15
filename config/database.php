6c6c0c79a8934bfa466a63b242b8022e91c7c179
BBahaeddine <bouslamabahaeddine123@gmail.com>
2018-03-15 10:08:20 +0100

    Change DB Config

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'sql2.freemysqlhosting.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'sql2224199'),
+            'username' => env('DB_USERNAME', 'sql2224199'),
+            'password' => env('DB_PASSWORD', 'tM9!nI4*'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
